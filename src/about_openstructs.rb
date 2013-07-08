cd34f05e087e0cadaa96de50f8b197fcaaa512e9
smlance <stefan@lances.net>
2013-07-08 09:28:24 -0400

    Added about_openstructs.rb

---
+++ src/about_openstructs.rb
@@ -0,0 +1,55 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+# For OpenStructs
+require 'ostruct'
+
+class AboutOpenStructs < Neo::Koan
+  
+  # Might be better to put this in about_hashes.rb.
+  def test_openstructs_behave_like_hashes
+    person = OpenStruct.new
+    person.name = "Joe"
+    person.age = 25
+    person.siblings = 3
+    
+    person_info = person.each_pair { |key, value| print key, ", ", value, "; "  }
+
+    assert_equal __("name, Joe; age, 20; siblings, 3;"), person_info
+  end
+
+  def test_openstructs_understand_symbols
+    person = OpenStruct.new
+    person.name = "Joe"
+    person.age = 25
+    person.siblings = 3
+
+    assert_equal __("Joe"), person[:name]
+
+    person[:eye_color] = "brown"
+    assert_equal __("brown"), person[:eye_color]
+  end
+
+  def test_openstructs_can_be_initialized_like_hashes
+    person = OpenStruct.new(:name => "Joe", :age => 25, :siblings => 3)
+
+    assert_equal __(25), person[:age]
+  end
+
+  def test_hash_conversions_from_openstruct
+    person = OpenStruct.new(:name => "Joe", :age => 25, :siblings => 3)
+
+    assert_equal __(Hash), person.to_h.class
+    assert_equal __(Hash), Hash(person).class
+  end
+
+  # def test_hash_conversions_from_struct
+  #   person = Struct.new(:name => "Joe", :age => 25, :siblings => 3)
+
+  #   assert_equal __(Hash), person.to_h.class
+  #   assert_equal __(Hash), Hash(person).class
+  # end
+  
+  # It turns out Structs don't behave this way, and we need to add them
+  # separately (maybe to a file of their own?)
+
+end
