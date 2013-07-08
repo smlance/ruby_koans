59b55f3d35f5dee91963f4168c6c0b4d49dd7653
smlance <stefan@lances.net>
2013-07-08 11:23:52 -0400

    Fixed incorrect indentation levels.

--- src/about_refinements.rb
+++ src/about_refinements.rb
@@ -2,51 +2,51 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutRefinements < Neo::Koan
 
-    class Dog
+  class Dog
 
-      attr_reader :name
+    attr_reader :name
 
-      def initialize
-        @name = "Fido"
-      end
+    def initialize
+      @name = "Fido"
+    end
 
+    def bark
+      "WOOF"
+    end
+
+    def here
+      :in_object
+    end
+  end
+
+  module ActsLikeTiger
+
+    refine Dog do
       def bark
-        "WOOF"
+        "ROAR"
       end
 
-      def here
-        :in_object
+      def runs_fast
+        true
       end
-    end
-
-    module ActsLikeTiger
-
-      refine Dog do
-        def bark
-          "ROAR"
-        end
-
-        def runs_fast
-          true
-        end
-      end
-
-    end
-
-    def test_refinements_not_normally_visible
-      fido = Dog.new
-
-      assert_equal __("WOOF"), fido.bark
-      assert_equal __(false), fido.runs_fast
-      # TODO: Need to implement NoMethodError in this part of the code...
-    end
-
-    def test_refinements_require_module_use
-      using ActsLikeTiger
-      fido = Dog.new
-
-      assert_equal __("ROAR"), fido.bark
-      assert_equal __(true), fido.runs_fast
     end
 
   end
+
+  def test_refinements_not_normally_visible
+    fido = Dog.new
+
+    assert_equal __("WOOF"), fido.bark
+    assert_equal __(false), fido.runs_fast
+    # TODO: Need to implement NoMethodError in this part of the code...
+  end
+
+  def test_refinements_require_module_use
+    using ActsLikeTiger
+    fido = Dog.new
+
+    assert_equal __("ROAR"), fido.bark
+    assert_equal __(true), fido.runs_fast
+  end
+
+end
