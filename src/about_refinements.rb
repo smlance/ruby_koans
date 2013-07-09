e4f7a7763ad9551dfa90df5f411567a7a08d1243
smlance <stefan@lances.net>
2013-07-09 14:58:24 -0400

    Fixed the about_refinements.rb file (partly by removing the module and class declarations from the testing class and putting them at the top level).

--- src/about_refinements.rb
+++ src/about_refinements.rb
@@ -1,52 +1,76 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-class AboutRefinements < Neo::Koan
+# Maybe include a real monkeypatching instance (like refining String
+# or FixNum for some purpose.)
 
-  class Dog
+module ActsLikeTiger
+  # We will add to this module below.
+end
 
-    attr_reader :name
+class Dog
+  
+  include ActsLikeTiger
+  # What will the ActsLikeTiger module do to instances of Dog?
 
-    def initialize
-      @name = "Fido"
-    end
+  attr_reader :name
 
+  def initialize
+    @name = "Fido"
+  end
+
+  def bark
+    "WOOF"
+  end
+
+  def here
+    :in_object
+  end
+end
+
+class SpecificDog < Dog
+
+end
+
+module ActsLikeTiger
+
+  refine SpecificDog do
     def bark
-      "WOOF"
+      "ROAR"
     end
 
-    def here
-      :in_object
+    def runs_fast
+      "I do run fast!"
     end
   end
 
-  module ActsLikeTiger
+end
 
-    refine Dog do
-      def bark
-        "ROAR"
-      end
+using ActsLikeTiger
+# What happens if ActsLikeTiger is not used?
 
-      def runs_fast
-        true
-      end
-    end
-
-  end
+class AboutRefinements < Neo::Koan
 
   def test_refinements_not_normally_visible
     fido = Dog.new
 
     assert_equal __("WOOF"), fido.bark
-    assert_equal __(false), fido.runs_fast
-    # TODO: Need to implement NoMethodError in this part of the code...
+    assert_equal __(false), (fido.respond_to? :runs_fast)
   end
 
-  def test_refinements_require_module_use
-    using ActsLikeTiger
-    fido = Dog.new
+  def test_refinements_visible_to_refined_classes_when_using_refinements
+    fido = SpecificDog.new
 
     assert_equal __("ROAR"), fido.bark
-    assert_equal __(true), fido.runs_fast
+    assert_equal __("I do run fast!"), fido.runs_fast
+
+    assert_equal __(false), (fido.respond_to? :runs_fast)
+
+    # THINK ABOUT IT:
+    #
+    # The instance of the SpecificDog class makes a claim about whether
+    # it responds to #runs_fast, and it then tries to respond to it.
+    # Why are the claim and the action not in agreement with one another?
+
   end
 
 end
