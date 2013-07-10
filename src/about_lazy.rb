237243e693c41471e812a8b8214e941657fb387b
Isaac Rosenberg <irosenb7@gmail.com>
2013-07-10 15:00:53 -0400

    large range with or w/o lazy

--- src/about_lazy.rb
+++ src/about_lazy.rb
@@ -1,5 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+# Our universe is infinite in its complexity. 
+# Yet we as humans have finite energy. 
+# In order to deal with an infinite universe, we must be lazy. 
+# Expend as little energy as possible, yet gain as much as possible. 
+# That is true enlightenment. 
+
 class AboutLazyEnumeration < Neo::Koan
 
   class Num
@@ -11,38 +17,40 @@ class AboutLazyEnumeration < Neo::Koan
       end
     end
   end
+ 
+  def test_large_range_without_lazy # Move to different file? To show time?
+    range = (1..19999999)
+    array = range.select{|n| n % 2 == 0 }.first(5)
+    assert_equal __([2,4,6,8,10]), array
+    # Meditate on how long it took to return an answer. 
+    # We can surely be lazier than that.
+  end
+
+  def test_large_range_with_lazy 
+    range = (1..19999999).__(lazy)
+    array = range.select{|n| n % 2 == 0 }.first(5)
+    assert_equal __([2,4,6,8,10]), array
+    # Much better timing. 
+  end
 
   def test_infinite_range 
     range = 1..Float::INFINITY
-    collection = range.lazy.collect{ |x| x }.first(5)
+    # To be lazy or not to be lazy, that is the question.
+    collection = range.__(lazy).collect{ |x| x }.first(5)
     # collection is an array
     assert_equal __([1, 2, 3, 4, 5]), collection
   end 
 
-  def test_files
-    file = 'example_file.txt'
-    lines = File.open(file).lazy.each.map(&:chomp).reject(&:empty?).take(3).force
-    assert_equal __(["this", "is", "a"]), lines
-  end
-
   def test_size
     array = [1, 2, 3, 4]
-    size = array.cycle(4).size
-    assert_equal __(16), size
+    size = (array * 2).size
+    assert_equal __(8), size
   end
 
 	def test_lazy_select
     num = Num.new
     enums = []
-    num.enum_for(:even).lazy.select{|i| i < 3 }.take(2).each{|i| enums << i} 
+    enums = num.enum_for(:even).__(lazy).select{|i| i < 3 }.first(2)
     assert_equal __([0, 2]), enums
-  end
-
-  def test_lazy_again
-    num = Num.new
-    enums = []
-    # How can we assure that this does not go into an infinite loop?
-    num.enum_for(:even).__(lazy).select{|i| i < 5 }.take(3).each{|i| enums << i} 
-    assert_equal __([0, 2, 4]), enums
   end
 end
