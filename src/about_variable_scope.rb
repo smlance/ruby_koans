be1265d9e77b8bff8668ad4b67eaa80843e2ea27
smlance <stefan@lances.net>
2013-07-11 15:27:12 -0400

    Added comments and capitalized strings in about_variable_scope.rb. Added a test to help teach global vars.

--- src/about_variable_scope.rb
+++ src/about_variable_scope.rb
@@ -1,18 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutVariableScope < Neo::Koan
-		def bark
-      barking = "ruff!"  
-		end
 
-	def test_barking_is_not_available_in_the_current_scope
-		assert_raise(___(NameError)) do
-			barking
-		end
-	end
+  def bark
+    noise = "RUFF"
+  end
 
-  def test_we_can_get_barking_by_calling_def
-    assert_equal __("ruff!"), bark
+  def test_noise_is_not_available_in_the_current_scope
+
+    assert_raise(___(NameError)) do
+      noise
+    end
+
+  end
+
+  def test_we_can_get_noise_by_calling_method
+    assert_equal __("RUFF"), bark
   end
 
   inaccessible = "Outside our universe"
@@ -24,11 +27,12 @@ class AboutVariableScope < Neo::Koan
   # ------------------------------------------------------
 
   def test_blocks_can_access_variables_outside_scope 
-    test = "hi"
+    test = "Hi"
     (1..2).each do
-      test = "hey"
+      test = "Hey"
     end
-    assert_equal __("hey"), test    
+
+    assert_equal __("Hey"), test    
   end
 
   def test_block_variables_cannot_be_accessed_outside_scope
@@ -42,8 +46,11 @@ class AboutVariableScope < Neo::Koan
 
   class Mouse
     @@total = 0
+    # Class variables are prefixed with two '@' characters.
+
     def initialize(n)
-      @name = n 
+      @name = n
+      # Instance variables are prefixed with one '@' character.
       @@total += 1 
     end
 
@@ -57,8 +64,8 @@ class AboutVariableScope < Neo::Koan
   end
 
   def test_instance_variable 
-    oscar = Mouse.new("oscar")
-    assert_equal __("oscar"), oscar.name 
+    oscar = Mouse.new("Oscar")
+    assert_equal __("Oscar"), oscar.name 
   end
 
   def test_class_variable
@@ -72,21 +79,36 @@ class AboutVariableScope < Neo::Koan
 
   # ------------------------------------------------------
   
-  $anywhere = "anywhere"
-  def test_global_variables_can_be_accessed_from_anywhere
-    assert_equal __("anywhere"), $anywhere    
+  $anywhere = "Anywhere"
+  # Global variables are prefixed with the '$' character.
+
+  def test_global_variables_can_be_accessed_from_any_scope
+    assert_equal __("Anywhere"), $anywhere    
   end
 
-  def test_global_variables_can_be_changed_from_anywhere
-    # from within a def
-    $anywhere = "here"
-    assert_equal __("here"), $anywhere
+  def test_global_variables_can_be_changed_from_any_scope
+    # From within a method
+    $anywhere = "Here"
+    assert_equal __("Here"), $anywhere
+  end
 
-    # from within a block
+  def test_global_variables_retain_value_from_last_change
+    # What is $anywhere?
+    assert_equal __("Here"), $anywhere
+  end
+
+  def test_global_variables_can_be_changed_from_any_scope_2
+    # From within a block
     (1..2).each do
-      $anywhere = "hey"
+      $anywhere = "Hey"
     end
-    assert_equal __("hey"), $anywhere
+
+    assert_equal __("Hey"), $anywhere
   end
 
 end
+
+# THINK ABOUT IT:
+# 
+# What will $anywhere be down here, outside of the scope of the
+# AboutVariableScope class?
